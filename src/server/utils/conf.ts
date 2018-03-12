494bcc8a859006df21efc58d883119771806acce
implicitdef <implicitdef@users.noreply.github.com>
2018-03-12 13:28:44 +0100

    changed default port to 3333

--- src/server/utils/conf.ts
+++ src/server/utils/conf.ts
@@ -1,13 +1,18 @@
 function reverse(s: string): string {
-    return s.split("").reverse().join("");
+  return s
+    .split("")
+    .reverse()
+    .join("");
 }
 
-export const googleClientId = reverse("moc.tnetnocresuelgoog.sppa.8ro4a1m8c1e349slnqhhti7nurp81l2o-412795760244");
+export const googleClientId = reverse(
+  "moc.tnetnocresuelgoog.sppa.8ro4a1m8c1e349slnqhhti7nurp81l2o-412795760244"
+);
 export const db = process.env.DATABASE_URL || {
   host: "127.0.0.1",
   user: "manu",
   password: "",
-  database: "tv_shows_calendar",
+  database: "tv_shows_calendar"
 };
 export const defaultShowsIds = [
   1399, // Game of Thrones
@@ -19,6 +24,6 @@ export const defaultShowsIds = [
   1424, // Orange is the new black
   1412, // Arrow
   2190, // South park
-  61733, // Empire
+  61733 // Empire
 ];
-export const port = process.env.PORT || 3000;
+export const port = process.env.PORT || 3333;
