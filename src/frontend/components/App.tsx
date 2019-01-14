7eb2557ac30344618f7edd73dfea170f1e21c756
implicitdef <implicitdef@users.noreply.github.com>
2019-01-14 17:23:25 +0100

    remove integration page

--- src/frontend/components/App.tsx
+++ src/frontend/components/App.tsx
@@ -1,14 +1,9 @@
 import * as React from "react";
 import { Provider } from "react-redux";
-import IntegrationPage from "tv/frontend/components/IntegrationPage";
 import TruePage from "tv/frontend/components/TruePage";
 import Store from "tv/frontend/redux/store";
 
-const displayIntegrationPage = false;
-
-export const rootElement = displayIntegrationPage ? (
-  <IntegrationPage />
-) : (
+export const rootElement = 
   <Provider store={Store}>
     <TruePage />
   </Provider>
